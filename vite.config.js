3ec38160f880ff47e5441d7006c025b66e0f782c
unknown <sarontadele2019@gmail.com>
2024-06-20 08:57:28 +0100

    folders and package is setup

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	server: {
+		host: "127.0.0.1", // Optional: Explicitly sets the host to IPv4 localhost
+		port: 3000, // Sets the port to 3000
+	},
+});
